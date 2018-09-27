0033548680afa5f2df2014efaf5dc6789b667f10
ankur2018bardhan <43574313+ankur2018bardhan@users.noreply.github.com>
2018-09-27 13:42:42 +0530

    Update index.php

--- test78/index.php
+++ test78/index.php
@@ -2,6 +2,6 @@
 
 echo "<H1>Hello World</H1>";
 
-echo "<h4>A repository is usually used to organize a single project. Repositories can contain folders and files, images, videos, spreadsheets, and data sets – anything your project needs. We recommend including a README, or a file with information about your project. GitHub makes it easy to add one at the same time you create your new repository. It also offers other common options such as a license file.</h4>";
+echo "<h4>A repository is usually used to organize a single project. Rrrrrrepositories can contain folders and files, images, videos, spreadsheets, and data sets – anything your project needs. We recommend including a README, or a file with information about your project. GitHub makes it easy to add one at the same time you create your new repository. It also offers other common options such as a license file.</h4>";
 
 ?>
