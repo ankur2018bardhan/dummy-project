3bf2dcfecf9b43a33018ad53c169b87496134506
ankur2018bardhan <43574313+ankur2018bardhan@users.noreply.github.com>
2018-09-27 15:16:19 +0530

    new21

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<H1>Hello World</H1>";
+
+echo "<h4>A repository is usually used to organize a single project. Repositories can contain folders and files, images, videos, spreadsheets, and data sets – anything your project needs. We recommend including a README, or a file with information about your project. GitHub makes it easy to add one at the same time you create your new repository. It wassssss also aaaa offers other common options such as a license file.</h4>";
+
+?>
